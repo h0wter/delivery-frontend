bc254dcbc11dbcd3dca43b75dde356f2c5bd27fa
h0wter <howter99@gmail.com>
2023-06-06 19:42:46 +0300

    add: GlobalStyle and remove index.css

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -1,5 +1,7 @@
-@import "../node_modules/modern-normalize/modern-normalize.css";
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
 
+export const GlobalStyle = createGlobalStyle`
 :root {
   font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
   line-height: 1.5;
@@ -12,9 +14,14 @@
   -webkit-text-size-adjust: 100%;
 }
 
-body,
-p {
-  margin: 0;
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+	margin: 0;
 }
 
 ul {
@@ -22,3 +29,14 @@ ul {
   padding: 0;
   list-style: none;
 }
+
+a {
+	text-decoration: none;
+}
+
+img {
+	display: block;
+	max-width: 100%;
+	height: auto;
+}
+`;
