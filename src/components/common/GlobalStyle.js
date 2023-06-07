cad85edef467eee94569450eea503193d806556b
h0wter <howter99@gmail.com>
2023-06-07 19:24:30 +0300

    Update GlobalStyle.js

--- src/components/common/GlobalStyle.js
+++ src/components/common/GlobalStyle.js
@@ -3,15 +3,27 @@ import 'modern-normalize';
 
 export const GlobalStyle = createGlobalStyle`
 :root {
+  height: 100vh;
+  padding: 20px;
   font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
   line-height: 1.5;
   font-weight: 400;
-
+  background-color: #e1e1e1;
   font-synthesis: none;
   text-rendering: optimizeLegibility;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   -webkit-text-size-adjust: 100%;
+}
+
+body {
+  height: 100%;
+}
+
+div#root {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 
 p,
