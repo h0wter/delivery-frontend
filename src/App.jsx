6887e5f3f9fd5ac9434620bcc591c9a7b696123d
h0wter <howter99@gmail.com>
2023-06-07 19:26:35 +0300

    update: add routes to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { GlobalStyle } from './components/common/GlobalStyle';
+import { Layout } from './components/common/Layout';
+
+const HomePage = lazy(() => import('./pages/Home/Home'));
+const CartPage = lazy(() => import('./pages/Cart/Cart'));
+
 function App() {
   return (
-    <>
-      <p>main page</p>
-    </>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Route>
+      </Routes>
+      <Toaster position="bottom-center" />
+      <GlobalStyle />
+    </Suspense>
   );
 }
 
