22e2ced781006269707c84aa25e607e72a09b943
h0wter <howter99@gmail.com>
2023-06-12 18:57:39 +0300

    refactor CartList: move styles from box to styled

--- src/components/CartList/CartList.jsx
+++ src/components/CartList/CartList.jsx
@@ -1,6 +1,12 @@
 import { Box } from '../common/Box';
 import { Button } from '../common/Button/Button';
-import { CartImg, StyledInput, StyledItem } from './CartList.styled';
+import {
+  CartImg,
+  StyledInput,
+  StyledItem,
+  ItemInfo,
+  ItemPriceWrapper,
+} from './CartList.styled';
 
 export const CartList = ({
   cart,
@@ -13,22 +19,9 @@ export const CartList = ({
         {cart.map(({ _id, name, pictureUrl, price, quantity }) => (
           <StyledItem key={_id}>
             <CartImg src={pictureUrl} alt={name} />
-            <Box
-              display="flex"
-              flexDirection="column"
-              justifyContent="center"
-              maxWidth="220px"
-              textAlign="center"
-            >
+            <ItemInfo>
               <p>{name}</p>
-              <Box
-                mt={3}
-                mb={3}
-                display="grid"
-                gridGap={2}
-                gridTemplateColumns="1fr 1fr"
-                alignItems="baseline"
-              >
+              <ItemPriceWrapper>
                 <p>Price: {price}$</p>
                 <StyledInput
                   type="number"
@@ -38,11 +31,11 @@ export const CartList = ({
                     handleQuantityChange(_id, parseInt(e.target.value))
                   }
                 />
-              </Box>
+              </ItemPriceWrapper>
               <Button onClick={() => handleRemoveButtonClick(_id)}>
                 Remove
               </Button>
-            </Box>
+            </ItemInfo>
           </StyledItem>
         ))}
       </ul>
