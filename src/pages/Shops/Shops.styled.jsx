15f15f5d5b3360fe4e8bf5900345657a0e92e8e0
h0wter <howter99@gmail.com>
2023-06-12 19:23:01 +0300

    refactor Shops: move styles from box to styled component

--- src/pages/Shops/Shops.styled.jsx
+++ src/pages/Shops/Shops.styled.jsx
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  display: grid;
+  grid-gap: 32px;
+  grid-template-columns: 3fr 7fr;
+  flex: 1;
+`;
+
+export const ShopsListContainer = styled.div`
+  padding: 16px 32px;
+  border-radius: 10px;
+  border: 1px solid #808080;
+`;
+
+export const ShopMenuContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 16px 32px;
+  border-radius: 10px;
+  border: 1px solid #808080;
+`;
+
 export const StyledModal = styled.div`
   display: flex;
   flex-direction: column;
